2a21c0509817e9d5547459483872487f733d5ad5
Zhen, He <zhe@litle.com>
2013-05-09 11:15:29 -0400

    delete test folder to resolve build path issue

--- sdk-parser/Mytest/ReadXMLFile.java
+++ sdk-parser/Mytest/ReadXMLFile.java
@@ -13,7 +13,7 @@ public class ReadXMLFile {
  
     try {
  
-	File fXmlFile = new File("/usr/local/litle-home/vchouhan/Desktop/NewStaff.xml");
+	File fXmlFile = new File("/usr/local/litle-home/zhe/parsePDF/xml_XML_Ref_elements.xml");
 	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 	Document doc = dBuilder.parse(fXmlFile);
