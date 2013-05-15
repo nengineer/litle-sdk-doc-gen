2cf5bddaafd31b28c54c1ddd482011005bfded44
Zhen, He <zhe@litle.com>
2013-05-15 14:53:35 -0400

    extract the optional attributes like Type, maxLength, and minLength.

--- sdk-parser/src/extractXML/DIVElement.java
+++ sdk-parser/src/extractXML/DIVElement.java
@@ -2,6 +2,8 @@ package extractXML;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -9,19 +11,24 @@ import org.w3c.dom.NodeList;
 
 
 public class DIVElement {
-	
+
 	private String eleName;
 	private String eleType;
+	private String descrip;
+	private HashMap<String, String> attrs;
 	private ArrayList<String> parentElements;
-	private HashMap<String, String> childElements; 
-	//private ArrayList<Attribute> attributes;
-	
-	public DIVElement(){
+	private HashMap<String, String> childElements;
+
+	// private ArrayList<Attribute> attributes;
+
+	public DIVElement() {
 		this.eleName = "";
+		this.descrip = "";
+		this.attrs = new HashMap<String, String>();
 		this.parentElements = new ArrayList<String>();
 		this.childElements = new HashMap<String, String>();
 	}
-	
+
 	public String getEleName() {
 		return eleName;
 	}
@@ -41,12 +48,12 @@ public class DIVElement {
 	public ArrayList<String> getParentElements() {
 		return parentElements;
 	}
-	
+
 	public void setParentElements(ArrayList<String> parentElements) {
 		this.parentElements = parentElements;
 	}
-	
-	public void addEleToParentElements(String e){
+
+	public void addEleToParentElements(String e) {
 		this.parentElements.add(e);
 	}
 
@@ -57,133 +64,223 @@ public class DIVElement {
 	public void setChildElements(HashMap<String, String> childElements) {
 		this.childElements = childElements;
 	}
-	
-	public void addEleToChildElements(String e, String option){
+
+	public void addEleToChildElements(String e, String option) {
 		this.childElements.put(e, option);
 	}
 
+	public String getDescrip() {
+		return descrip;
+	}
+
+	public void setDescrip(String descrip) {
+		this.descrip = descrip;
+	}
 	
+	public void appendDescrip(String newDescrip){
+		this.descrip = this.descrip + newDescrip;
+	}
+
+
+	
+	public void addEleToAttrs(String key, String value) {
+		this.attrs.put(key, value);
+	}
+
+	public HashMap<String, String> getAttrs() {
+		return attrs;
+	}
+
+	public void setAttrs(HashMap<String, String> attrs) {
+		this.attrs = attrs;
+	}
+
 	/**
 	 * process the whole block of XML element
+	 * 
 	 * @param div
 	 */
-	public void processDIV(Element div){
+	public void processDIV(Element div) {
 		this.extractItemName(div);
 		System.out.println("Item Name: " + this.eleName);
-		this.extractSubHeaderElement(div);
-		for(String s: parentElements){
-			System.out.println("Parent Element: "+s);
+		System.out.println("Description: " + this.descrip);
+		for (Entry<String, String> i : attrs.entrySet()) {
+			System.out.println("Key Attributes: " + i.getKey() + ", "
+					+ i.getValue());
 		}
-		for(Entry<String, String> i: childElements.entrySet()){
-			System.out.println("Child Element: "+i.getKey()+", "+i.getValue());
+		this.extractSubHeaderElement(div);
+		for (String s : parentElements) {
+			System.out.println("Parent Element: " + s);
+		}
+		for (Entry<String, String> i : childElements.entrySet()) {
+			System.out.println("Child Element: " + i.getKey() + ", "
+					+ i.getValue());
 		}
 	}
-	
+
 	/**
 	 * extract the name of the XML element
+	 * 
 	 * @param div
 	 */
-	public void extractItemName(Element div){
-		NodeList nodes = div.getElementsByTagName(XMLLookUpStrings.H1_HEAD1);  
-		for(int i=0; i< nodes.getLength(); i++){
-				Node eleA = nodes.item(i);
-				if(eleA.getNodeType() == Node.ELEMENT_NODE){
-					String itemName = trimString(eleA.getTextContent());
-					if(itemName != ""){
-						this.setEleName(itemName);
-					}
-					extractDescription(eleA);
+	public void extractItemName(Element div) {
+		NodeList nodes = div.getElementsByTagName(XMLLookUpStrings.H1_HEAD1);
+		for (int i = 0; i < nodes.getLength(); i++) {
+			Node eleA = nodes.item(i);
+			if (eleA.getNodeType() == Node.ELEMENT_NODE) {
+				String itemName = trimWhiteSpace(eleA.getTextContent());
+				if (itemName != "") {
+					this.setEleName(itemName);
 				}
+				extractDescription(eleA);
+			}
 		}
 	}
-	
-	public void extractDescription(Node node){
-		Node sibling = node.getNextSibling();
-		while(sibling != null) {
-		    if ( sibling.getNodeType() == Node.ELEMENT_NODE ) {
-		    	//System.out.println(sibling.getTextContent().replaceAll("\n", ""));
-		    	String s1 = sibling.getTextContent().replaceAll("\n", "");
-		    	System.out.println(s1.replaceAll("\b[a-zA-Z0-9_],|, and |, ", "\n@"));
-		    	formatDescription(sibling.getTextContent());
-		    	break;
-		    }
-		    sibling = sibling.getNextSibling();
-		}
-		
-	}
-	
-	private String formatDescription(String raw){
-		return "";
-	}
-	
+
 	/**
-	 * extract sub header elements including parentElements, childElements, and attributes
+	 * extract the brief description of the element. It can be one or two short paragraphs
+	 * @param node
+	 */
+	public void extractDescription(Node node) {
+		Node sibling = node.getNextSibling();
+		while (sibling != null ) {
+			if (sibling.getNodeType() == Node.ELEMENT_NODE) {
+				Element se = (Element) sibling;
+				if(se.getTagName().equals(XMLLookUpStrings.BP_BODY1) && !stopForDescrip(se.getTextContent())){
+					String s1 = trimNewLine(se.getTextContent());
+					appendDescrip(s1);
+				}
+				else{
+					break;
+				}
+			}
+			sibling = sibling.getNextSibling();
+		}
+		extractTypeOptional(sibling);
+
+	}
+
+	public void extractTypeOptional(Node sibling) {
+		while (sibling != null) {
+			if (sibling.getNodeType() == Node.ELEMENT_NODE) {
+				Element se = (Element) sibling;
+				if(se.getTagName().equals(XMLLookUpStrings.BP_BODY1)){
+					String s1 = trimNewLine(se.getTextContent());
+					formatTypes(s1);
+					
+				}
+				break;
+			}
+			sibling = sibling.getNextSibling();
+		}
+	}
+
+	/**
+	 * extract sub header elements including parentElements, childElements, and
+	 * attributes
+	 * 
 	 * @param div
 	 */
-	public void extractSubHeaderElement(Element div){
-		NodeList nodes = div.getElementsByTagName(XMLLookUpStrings.SH1_SUBHEAD1);
-		for (int i=0; i < nodes.getLength(); i++){
+	public void extractSubHeaderElement(Element div) {
+		NodeList nodes = div
+				.getElementsByTagName(XMLLookUpStrings.SH1_SUBHEAD1);
+		for (int i = 0; i < nodes.getLength(); i++) {
 			Node node = nodes.item(i);
-			String nodeContent = trimString(node.getTextContent());
-			if(nodeContent.equals(XMLLookUpStrings.PARENTELEMENTS)){
+			String nodeContent = trimWhiteSpace(node.getTextContent());
+			if (nodeContent.equals(XMLLookUpStrings.PARENTELEMENTS)) {
 				extractParentElements(node);
 			}
-			if(nodeContent.equals(XMLLookUpStrings.CHILDELEMENTS)){
+			if (nodeContent.equals(XMLLookUpStrings.CHILDELEMENTS)) {
 				extractChildElements(node);
 			}
 		}
 	}
-	
 
-	public void extractParentElements(Node node){
+	/**
+	 * extract parent elements 
+	 * @param node
+	 */
+	public void extractParentElements(Node node) {
 		Node sibling = node.getNextSibling();
-		while(sibling != null) {
-		    if ( sibling.getNodeType() == Node.ELEMENT_NODE ) {
-		    	Element pe = (Element)sibling;
-				NodeList parents = pe.getElementsByTagName(XMLLookUpStrings.COLOR);
-				for (int i=0; i<parents.getLength(); i++){
+		while (sibling != null) {
+			if (sibling.getNodeType() == Node.ELEMENT_NODE) {
+				Element pe = (Element) sibling;
+				NodeList parents = pe
+						.getElementsByTagName(XMLLookUpStrings.COLOR);
+				for (int i = 0; i < parents.getLength(); i++) {
 					Node parent = parents.item(i);
-					String parentName = trimString(parent.getTextContent());
-					if(parentName != ""){
+					String parentName = trimWhiteSpace(parent.getTextContent());
+					if (parentName != "") {
 						this.addEleToParentElements(parentName);
 					}
 				}
-		    	break;
-		    }
-		    sibling = sibling.getNextSibling();
+				break;
+			}
+			
+			sibling = sibling.getNextSibling();
 		}
 	}
-	
-	public void extractChildElements(Node node){
+
+	/**
+	 * extract child elements
+	 * @param node
+	 */
+	public void extractChildElements(Node node) {
 		Node sibling = node.getNextSibling();
-		int  count = 0; //there are at most two sets of childElements: required and optional
-		while(sibling != null) {
-		    if ( sibling.getNodeType() == Node.ELEMENT_NODE ) {
-		    	count++;
-		    	Element ce = (Element)sibling;
-		    	String option = trimString(ce.getTextContent()).replaceAll(":.*", "");
-				NodeList children = ce.getElementsByTagName(XMLLookUpStrings.COLOR);
-				for (int i=0; i<children.getLength(); i++){
+		int count = 0; // there are at most two sets of childElements: required
+						// and optional
+		while (sibling != null) {
+			if (sibling.getNodeType() == Node.ELEMENT_NODE) {
+				count++;
+				Element ce = (Element) sibling;
+				String option = formatChildElementsOption(ce.getTextContent());
+				NodeList children = ce
+						.getElementsByTagName(XMLLookUpStrings.COLOR);
+				for (int i = 0; i < children.getLength(); i++) {
 					Node child = children.item(i);
-					String childName = trimString(child.getTextContent());
-					if(childName != ""){
+					String childName = trimWhiteSpace(child.getTextContent());
+					if (childName != "") {
 						this.addEleToChildElements(childName, option);
 					}
 				}
-				if(count>=2){
+				if (count >= 2) {
 					break;
 				}
-				
-		    }
-		    sibling = sibling.getNextSibling();
+
+			}
+			sibling = sibling.getNextSibling();
 		}
-	}
-	
-	private String trimString(String raw){
-		return raw.replaceAll("\\s","");
 	}
 
 	
+	private String formatChildElementsOption(String s) {
 	
+		return trimWhiteSpace(s).replaceAll(":.*", "");
+	}
+	
+	private void formatTypes(String s){
+		String [] entries = s.split(";");
+		
+		for (String entry: entries){
+			String key = entry.replaceAll(" = .*", "");
+			String value = entry.replaceAll(".* = ", "");
+			this.addEleToAttrs(key, value);
+		}
+	
+	}
+
+	private String trimWhiteSpace(String raw) {
+		return raw.replaceAll("\\s", "");
+	}
+
+	private String trimNewLine(String raw) {
+		return raw.replaceAll("\\n", "");
+	}
+	
+	private boolean stopForDescrip(String match){
+		Pattern p = Pattern.compile(".* = .*");
+		Matcher m = p.matcher(match);
+		return m.find();
+	}
 
 }
--- sdk-parser/src/extractXML/XMLLookUpStrings.java
+++ sdk-parser/src/extractXML/XMLLookUpStrings.java
@@ -5,6 +5,7 @@ public class XMLLookUpStrings {
 	final static String DIV = "DIV";
 	final static String H1_HEAD1 = "H1-Head1";
 	final static String SH1_SUBHEAD1 = "SH1-Subhead1";
+	final static String BP_BODY1 = "BP-Body";
 	final static String PARENTELEMENTS = "ParentElements:";
 	final static String CHILDELEMENTS = "ChildElements:";
 	final static String COLOR = "Color";
